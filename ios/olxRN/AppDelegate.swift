407d36e886012fd73a0d0c08dd08ac78b58b6d01
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-10-23 16:39:37 +0530

    Add PNG sticker images to drawable-mdpi resources

--- ios/olxRN/AppDelegate.swift
+++ ios/olxRN/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "olxRN",
+      withModuleName: "Bookosaurs",
       in: window,
       launchOptions: launchOptions
     )
